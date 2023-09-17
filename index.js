074c98d5155024875414ecc0cb508b8453227345
emmanuel-kipngeno <immanuelkipngeno7387@gmail.com>
2023-09-17 13:06:16 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-function addingEventListener() {
-}
+// Get a reference to the button element by its id
+const myButton = document.getElementById("myButton");
+
+// Add an event listener for a click event
+myButton.addEventListener("click", function() {
+  // This function will be executed when the button is clicked
+  alert("Button clicked!");
+});
+
